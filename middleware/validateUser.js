bff87a252b465c717009516c63c41bbde2cd2a6d
B Severino <biancasev@gmail.com>
2020-01-15 17:00:46 -0500

    Completed validateUser function

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,7 +1,8 @@
 function validateUser(req, res, next) {
-    if (req.body.length === 0) {
+    const body = req.body
+    if (Object.keys(body).length === 0) {
         res.status(400).json({ message: 'Missing user data.' })
-    } else if (!req.body.name) {
+    } else if (!body.name) {
         res.status(400).json({ message: 'Missing required name field.' })
     } else {
         next()
