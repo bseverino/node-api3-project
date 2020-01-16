8c70c34bb8f48aa4c39ae544b01e0e752ddab2ec
B Severino <biancasev@gmail.com>
2020-01-16 15:37:26 -0500

    Added .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const server = require('./server.js')
 
-const port =  process.env.PORT || 4000
+const port =  process.env.PORT
 
 server.listen(port, () => {
     console.log(`server is listening on port ${port}`)
